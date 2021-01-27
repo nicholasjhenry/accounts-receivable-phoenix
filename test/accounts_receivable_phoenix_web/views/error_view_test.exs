54e7ed6d4037b32834fa8a0a3a1c61195d6151fc
Nicholas Henry <nicholas.henry@gmail.com>
2021-01-27 10:01:18 -0500

    Format
    mix format mix.exs "lib/**/*.{ex,exs}" "test/**/*.{ex,exs}"

--- test/accounts_receivable_phoenix_web/views/error_view_test.exs
+++ test/accounts_receivable_phoenix_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule AccountsReceivablePhoenixWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(AccountsReceivablePhoenixWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(AccountsReceivablePhoenixWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
